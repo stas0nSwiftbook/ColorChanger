be3ed62c3ee4082e00a35eb84bf70d31028318f6
Stanislav Buinovsky <sbuinovsky@gmail.com>
2022-01-24 10:54:28 +0300

    Initial Commit

---
+++ ColorChanger/ColorChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerApp.swift
+//  ColorChanger
+//
+//  Created by Станислав Буйновский on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
